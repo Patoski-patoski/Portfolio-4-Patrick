b4b08aad0c83f9786c5e8d190d83ae6ce9f2bf99
Okafor Patrick <CodesByPatrick>
2022-07-05 00:30:23 +0100

    connection_mode

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function myView() {
           appear.style.display = "block";
           appear.addEventListener('mouseover', function () {
             let change = document.getElementById('index').style.color = '#ff9f29';
-            change.addEventListener('mouseover', function () {
+            appear.addEventListener('mouseover', function () {
             document.getElementById("services").style.color = "#ff9f29";
               
             })
@@ -40,24 +40,5 @@ function myView() {
 
 
 }
-
-//   linkOne = document
-//  .getElementById("linkOne")
-//     .addEventListener("mouseover", () => {
-//      linkOne = document.getElementById("linkOne").style.color = "#FF9F29";
-//       hove = document.getElementById("demo2").innerHTML =
-//         "Hello! Hover Me!";
-//       hove = document.getElementById("demo2").style.color = "#000";
-//       hove = document.getElementById("demo2").onmouseover = function () {
-//         document.getElementById("demo").style.color = "#FF9F29";
-//         hover = document.getElementById("display").style.display = "block";
-//       };
-//       dove = document.getElementById("demo2").onmouseout = function () {
-//         hover = document.getElementById("display").children[0].style.display = "flex";
-
-//         mee = document.getElementById('index');
-//         mee.style.display = "none";
-//       }
-//     });
 
 
