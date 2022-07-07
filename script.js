125e871717d2d8cb5ba5cb77517cb189de857b29
Okafor Patrick <CodesByPatrick>
2022-07-08 00:11:14 +0100

    30% completed

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
+let time = new Date();
+let realTime = document.getElementById('greet').innerHTML = time.getHours();
+
+if (realTime ==0 && realTime < 12) {
+  document.getElementById('greet').innerHTML = 'Good Morning,';
+  
+} else if (realTime >= 12 && realTime < 18) {
+  document.getElementById('greet').innerHTML = 'Good Afternoon,';
+}
+else {
+  document.getElementById('greet').innerHTML = 'Good evening,';
+}
+
 function myFunction() {
   const time = new Date().toLocaleTimeString();
   document.getElementById("demo").innerHTML = time;
 }
-
 function myView() {
   const linkOne = document.getElementById("linkOne");
-  linkOne.addEventListener('mouseover', function () {
-  
-    let reel = document.getElementById('demo2');
-    reel.innerHTML = 'Hello! Hover me';
-    
-    reel.onclick = function () { 
-      document.getElementById('demo2').style.color = '#ff9f29';
-      let appear = document.getElementById('display');
-      appear.style.display = 'block';
+  linkOne.addEventListener("mouseover", function () {
+    let hello = document.getElementById("demo2");
+    hello.innerHTML = "Hello! Hover me";
 
-    }
-        reel.onmouseover = function () {
-          document.getElementById("demo2").style.color = "#ff9f29";
-          let appear = document.getElementById("display");
-          appear.style.display = "block";
-          appear.addEventListener('mouseover', function () {
-            let change = document.getElementById('index').style.color = '#ff9f29';
-            appear.addEventListener('mouseover', function () {
-            document.getElementById("services").style.color = "#ff9f29";
-              
-            })
-          })
-          
+    hello.onmouseover = function () {
+      document.getElementById("demo2").style.color = "#ff9f29";
+      let appear = document.getElementById("display");
+      appear.style.display = "block";
+    
     };
-    
-    
 
- 
-
-
-
-  })
-
-
+      linkOne.onclick = function () {
+        document.getElementById("demo2").style.color = "#ff9f29";
+        let appear = document.getElementById("display");
+        appear.style.display = "block";
+        
+      };
+  });
 }
-
 
